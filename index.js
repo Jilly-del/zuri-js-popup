18818220f8c218c9652b36cc584fa4ded7a69d6d
Jilly-del <93799176+Jilly-del@users.noreply.github.com>
2022-06-25 11:40:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var yourName = prompt("what is your name?");
+var yourHeight = prompt("what's your height? ");
+var yourCountry = prompt("what is the name of your country?")
+
+ alert("Your name is " + yourName);
+
+alert ("Your height is " + yourHeight)
+
+alert("Your nationality is " + yourCountry);
